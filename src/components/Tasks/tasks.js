0d1bf7329cf2d2d5c724660666c54e731f2c70f6
Chase Hamby <c.chase.hamby@gmail.com>
2018-11-15 18:45:09 -0600

    everything working

--- src/components/Tasks/tasks.js
+++ src/components/Tasks/tasks.js
@@ -2,18 +2,18 @@
 // import $ from 'jquery';
 // import './tasks.scss';
 
-// const displayTasks = (tasks) => {
+// const displayTasks = () => {
 //   let newString = '';
 //   tasks.forEach((task) => {
 //     newString += `
 //       <div class="col-sm-2">
 //        <div class="saved-character" style="width: 18rem">
-//         <h3 class="placeholder">${task.task}</h3>
+//         <h3 class="placeholder">"${task}"</h3>
 //         <div class="placeholder">
-//           <img class="card-img" src="${task.imageUrl}">
+//           <img class="card-img" src="${imageUrl}">
 //         </div>
 //         <div class="placeholder">
-//           <h5 class="task-completed">${task.isCompleted}"</h5>
+//           <h5 class="task-completed">"${isCompleted}"</h5>
 //         </div> 
 //        </div>
 //       </div>  
