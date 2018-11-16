296bbe10ca17ff559a32cdd9cc27ef568ff3451c
Chase Hamby <c.chase.hamby@gmail.com>
2018-11-15 18:37:23 -0600

    all button functionality works and wip on task builder

---
+++ src/components/Tasks/tasks.js
@@ -0,0 +1,25 @@
+// import 'bootstrap';
+// import $ from 'jquery';
+// import './tasks.scss';
+
+// const displayTasks = (tasks) => {
+//   let newString = '';
+//   tasks.forEach((task) => {
+//     newString += `
+//       <div class="col-sm-2">
+//        <div class="saved-character" style="width: 18rem">
+//         <h3 class="placeholder">${task.task}</h3>
+//         <div class="placeholder">
+//           <img class="card-img" src="${task.imageUrl}">
+//         </div>
+//         <div class="placeholder">
+//           <h5 class="task-completed">${task.isCompleted}"</h5>
+//         </div> 
+//        </div>
+//       </div>  
+//     `;
+//   });
+//   $('#tasks').append(newString);
+// };
+
+// export default displayTasks;
