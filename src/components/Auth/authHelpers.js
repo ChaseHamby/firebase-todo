5fa46ea7f9e7599986bb6c9c430e4d0b3f35d390
Chase Hamby <c.chase.hamby@gmail.com>
2018-11-17 12:38:44 -0600

    styling

---
+++ src/components/Auth/authHelpers.js
@@ -0,0 +1,22 @@
+import $ from 'jquery';
+import firebase from 'firebase/app';
+
+const checkLoginStatus = () => {
+  firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+      $('#tasks').show();
+      $('#auth').hide();
+      $('#navbar-button-auth').hide();
+      $('#navbar-button-tasks').show();
+      $('#navbar-button-logout').show();
+    } else {
+      $('#tasks').hide();
+      $('#auth').show();
+      $('#navbar-button-auth').show();
+      $('#navbar-button-tasks').hide();
+      $('#navbar-button-logout').hide();
+    }
+  });
+};
+
+export default checkLoginStatus;
